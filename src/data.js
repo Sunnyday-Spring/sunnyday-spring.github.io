ca81043600f3bc185c07a7a629df49d14577cc55
Akbar Naser <akbarnaser24@gmail.com>
2026-07-26 23:52:04 +0700

    ่เสร็จแล้ว(มั่ง)

--- src/data.js
+++ src/data.js
@@ -3,7 +3,6 @@
 
 export const profile = {
   name: "Akbar Ali Ahmed Ahmed Naser",
-  role: "FULLSTACK DEVELOPER | FRONTEND DEVELOPER",
   availableFrom: "Start: November 2026 | Duration: 4 Months",
 };
 
